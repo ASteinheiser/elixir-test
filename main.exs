a25009bb4fc2a3b0d1ee3c2204a98043d76cf8b8
Andrew Steinheiser <me@iamandrew.io>
2018-07-18 21:11:53 -0700

    ...

--- main.exs
+++ main.exs
@@ -1,7 +1,22 @@
-# =====================
-# Elixir test
-# Andrew Steinheiser
-# July 2018
-# =====================
+# ==========================
+# Elixir Test
+# Andrew Steinheiser 2018
+# ==========================
 
-IO.puts "\n" <> "Hello. I'm Elixir..." <> "\n"
+IO.puts IO.ANSI.format(["\nHello. I'm ", :green, "Elixir\n"])
+IO.ANSI.reset()
+IO.puts IO.ANSI.format([:yellow, "I can do addition:"])
+IO.ANSI.reset()
+
+newFunc = fn a, b -> a + b end
+
+x = 7
+y = 4
+z = newFunc.(x, y)
+
+IO.inspect(x)
+IO.puts IO.ANSI.format([:blue, " + "])
+IO.inspect(y)
+IO.puts IO.ANSI.format([:blue, " = "])
+IO.inspect(z)
+IO.ANSI.reset()
